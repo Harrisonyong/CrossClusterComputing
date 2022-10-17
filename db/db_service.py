0ef59bc0a1c26a0172185a75f34f30b7bcfa56da
songquanheng <wannachan@outlook.com>
2022-10-17 16:57:30 +0800

    [ADD]修改db_service.py文件，添加批量插入接口，并且在该文件中引入了DBService的实例，以供其他文件导入

--- db/db_service.py
+++ db/db_service.py
@@ -5,6 +5,7 @@
 # date: 2022/10/14 Fri 15:27:45
 # description: 该服务用于与数据库进行交互
 
+from typing import List
 from sqlalchemy import create_engine, and_, or_
 from sqlalchemy.orm import sessionmaker
 
@@ -13,24 +14,35 @@ from utils.config import Configuration
 from sqlalchemy.ext.declarative import declarative_base
 
 dbConfig = Configuration.dbConfig()
-engine=create_engine(dbConfig["file"])
+engine = create_engine(dbConfig["file"])
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
+
 class DBService:
     '数据库服务，统一进行数据库的增删改查'
-    def query_all(self, Base):
+
+    def query_all(self, Base) -> list:
         session = Session()
         items = session.query(Base).all()
         session.close()
         return items
 
-    def addItem(self, Base):
+    def addItem(self, item):
         session = Session()
-        session.add(Base)
+        session.add(item)
         session.commit()
         session.close()
     
+    def addBatchItem(self, items):
+        session = Session()
+        session.add_all(items)
+        session.commit()
+        session.close()
+
     def dbConfig():
         return dbConfig
+
+
+dbService = DBService()
