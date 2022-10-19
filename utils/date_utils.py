ffaf604c1033c372dc9985b4aad68b54cafb88e1
songquanheng <wannachan@outlook.com>
2022-10-19 17:51:36 +0800

    [ADD]新建DateUtils类，封装日期时间有关操作

---
+++ utils/date_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+# author: songquanheng
+# email: wannachan@outlook.com
+# date: 2022/10/19 周三 15:54:16
+# description: 日期时间类
+
+from datetime import datetime
+
+
+class DateUtils:
+    """日期时间的辅助类"""
+    def nowStr(self):
+        now = datetime.now()
+        return now.strftime('%Y-%m-%d %H:%M:%S')
+
+dateUtils = DateUtils()
+
+if __name__ == '__main__':
+    print (dateUtils.nowStr())
